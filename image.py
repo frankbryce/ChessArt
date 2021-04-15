8e4d4bbeac315f5fbb62f42246ba7ad03e520a74
Frank Bryce <jonnyjack7@gmail.com>
2021-04-14 23:42:22 -0400

    Add ability for a piece to be more than a single pixel on a board image

--- image.py
+++ image.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 from board import *
+from enum import Enum
 import numpy as np
 from PIL import Image
 import os
@@ -18,15 +19,25 @@ DefaultColorMap = {
     Piece.K: (0, 0, 128),
 }
 
+class SplotchType(Enum):
+    SQUARE = 1
+
+class Splotch:
+    def __init__(self, typ: SplotchType = SplotchType.SQUARE, rad: int = 0) -> None:
+        self.typ = typ
+        self.rad = rad
+
 class BoardImage:
     def __init__(self,
             board: Board,
             border: int = 0,
             colorMap: ColorMap = DefaultColorMap,
+            splot: Splotch = Splotch(),
             outDir: str = 'images') -> None:
         self.board = board
         self.colorMap = colorMap
         self.outDir = outDir
+        self.splot = splot
         assert border >= 0
         self.box = (
             (
@@ -45,43 +56,48 @@ class BoardImage:
 
         # Lazy Load bmp generation when you add images together
         self.adds = []
-        self.subs = []
 
     def Add(self, board: Board) -> BoardImage:
         self.adds.append(img)
         return self
 
-    def Sub(self, board: Board) -> BoardImage:
-        self.subs.append(img)
-        return self
+    def _drawPiece(self, pixels, plmt: Plmt) -> pixels:
+        xc = plmt.pstn[0] - self.box[0][0]
+        yc = plmt.pstn[1] - self.box[0][1]
+        if self.splot.typ == SplotchType.SQUARE:
+            for rx in range(self.splot.rad*2+1):
+                for ry in range(self.splot.rad*2+1):
+                    x = min(max(xc - self.splot.rad + rx, 0), self.box[1][0]
+                        - self.box[0][0])
+                    y = min(max(yc - self.splot.rad + ry, 0), self.box[1][1]
+                        - self.box[0][1])
+                    pixels[x,y] = (
+                        (pixels[x,y][0] + self.colorMap[plmt.piece][0]) % 256,
+                        (pixels[x,y][1] + self.colorMap[plmt.piece][1]) % 256,
+                        (pixels[x,y][2] + self.colorMap[plmt.piece][2]) % 256
+                    )
+        else:
+            raise Exception(f"Unsupported splotch type {self.splot.typ}")
+        return pixels
+                
 
     def Draw(self, loc: str, rescale: int = 1) -> None:
         img = Image.new('RGB', (self.box[1][0] - self.box[0][0] + 1,
                                 self.box[1][1] - self.box[0][1] + 1))
         pixels = img.load() # Create the pixel map
         for plmt in self.board.Placements():
-            x = plmt.pstn[0] - self.box[0][0]
-            y = plmt.pstn[1] - self.box[0][1]
-            pixels[x,y] = self.colorMap[plmt.piece]
+            self._drawPiece(pixels, plmt)
         for add in self.adds:
             for plmt in add.Placements():
-                x = plmt.pstn[0] - self.box[0][0]
-                y = plmt.pstn[1] - self.box[0][1]
-                pixels[x,y] += self.colorMap[plmt.piece]
-        for sub in self.subs:
-            for plmt in sub.Placements():
-                x = plmt.pstn[0] - self.box[0][0]
-                y = plmt.pstn[1] - self.box[0][1]
-                pixels[x,y] -= self.colorMap[plmt.piece]
+                self._drawPiece(pixels, plmt)
         img.resize((img.size[0]*rescale, img.size[1]*rescale))
         os.makedirs(os.path.dirname(loc), exist_ok=True)
         img.save(loc)
 
 
-def main(imageLoc:str ='images/tmp.bmp', rescale: int=500):
+def main(imageLoc:str ='images/tmp.bmp', rescale: int=500) -> None:
     board = Board()
 
-    # pawns
     for x in range(25000):
         piece = Piece(np.random.randint(6)+1)
         board.Add(
